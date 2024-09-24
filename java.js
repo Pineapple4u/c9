b3944b673709e60158c60c8b063b336b6dfc9c0e
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-09-24 17:52:16 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+function operate(){
+    var input= document.getElementById("input").value; 
+    var no1= Number(document.getElementById("no1").value);
+    var no2= Number(document.getElementById("no2").value);
+    var a =document.getElementById("result");
+
+    switch (input){
+        case "+":
+        case "add":
+        case "addition":
+            a.innerHTML=no1+no2;
+            break;
+        case "-":
+        case "subtract":
+        case "sub":
+        case "subtraction":    
+            a.innerHTML=no1-no2;
+            break;    
+        case "x":
+        case "*":  
+        case "multiply":
+        case "mul":
+        case "multiplication":
+        case "X":
+            a.innerHTML=no1*no2;
+            break;
+
+        case "/":
+        case "divide":
+        case "division":
+        case "div":    
+           a.innerHTML=no1/no2;
+           break;
+    }
+}
